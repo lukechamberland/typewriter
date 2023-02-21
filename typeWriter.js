564ba5255068395fbfe2c04b36606b3954e4d21b
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-21 10:28:45 +0000

    First Commit

--- typeWriter.js
+++ typeWriter.js
@@ -1,4 +1,4 @@
-const sentence = "hello there from lighthouse labs\n";
+/*const sentence = "hello there from lighthouse labs\n";
 
 let timeout = 50;
 for (const char of sentence) {
@@ -6,4 +6,49 @@ for (const char of sentence) {
   process.stdout.write(char);
   }, timeout);
   timeout += 50;
-}
+} */
+ ////
+
+ /*setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, 1500);
+setTimeout(() => {
+  process.stdout.write('\r\|\n'); 
+}, 1500);*/
+
+////
+let myVar = 100
+let newVar = "|/-\"
+setTimeout(() => {
+  for (let ele of )
+  process.stdout.write('\r|   ');
+}, myVar);
+myVar += 200
